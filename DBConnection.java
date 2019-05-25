159d002e55210829b1a254cf80aba2a95d04b3fc
TPWeerakoon <47884289+TPWeerakoon@users.noreply.github.com>
2019-05-25 22:55:01 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,33 @@
+package Models;
+
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+
+public class DBConnection {
+		public static Connection createConnection() {
+			 Connection con = null;
+			 String url = "jdbc:mysql://localhost:8080/studentdb";
+			 String username = "root";
+			 String password = "root";
+			 
+			 try {
+				 try {
+					 Class.forName("com.mysql.jdbc.Driver");
+				 }
+				 catch (ClassNotFoundException e) {
+					 e.printStackTrace();
+				 }
+				 
+				 con = DriverManager.getConnection(url,username,password);
+				 System.out.println("Printing connection object "+con);
+			 }
+			 catch (Exception e) {
+				 e.printStackTrace();
+			 }
+			 return con;
+		}
+
+	}
+
+
+
